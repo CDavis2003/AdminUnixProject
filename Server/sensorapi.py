6ec16e0c81b1108c204a42f71083453dbfe48052
C Davis <davisconnorgc2003@gmail.com>
2025-03-12 21:33:14 -0400

    Created Server class to get utilization metrics from server using psutil library. created api using flask that will handle requests and send performance metrics in a json encoded dictionary. Currently UNTESTED!!!

---
+++ Server/sensorapi.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify
+from server import Server
+
+app = Flask(__name__)
+
+server = Server()
+
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    metrics = {
+        'cpuUsage': server.cpu_usage(),
+        'memoryAvailable': server.memory.available,
+        'swapAvailable': server.swap.available,
+        'diskFree': server.disk.free,
+        'ioWriteCount': server.diskIO.write_count,
+        'ioReadCount': server.diskIO.read_count,
+        'ioReadBytes': server.diskIO.read_bytes,
+        'ioWriteBytes': server.diskIO.write_bytes,
+        'netSentBytes': server.net.bytes_sent,
+        'netRecvBytes': server.net.bytes_recv,
+        'netSentPackets': server.net.packets_sent,
+        'netRecvPackets': server.net.packets_recv,
+        'netErrin': server.net.errin,
+        'netErrout': server.net.errout,
+    }
+    return jsonify(metrics)
+
+
+@app.route('/get_initial', methods=['GET'])
+def get_initial():
+    initial = {
+        'name' : server.name,
+        'cpuCores' : server.cpuCores,
+        'memTotal' : server.memory.total,
+        'swapTotal' : server.swap.total,
+        'diskTotal' : server.disk.total
+    }
+    return jsonify(initial)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
